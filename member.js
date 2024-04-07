a9721fb5b51c5c3f7bfc329beb8916959e5d3860
Brendan Swords <135817043+Brendan-Swords@users.noreply.github.com>
2024-04-07 14:30:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember()
+{
+    var member = document.getElementById("member").value;
+    var member = member.toLowerCase();
+    var member = member.replace(/\s/g, ''); // Add a closing parenthesis ')' at the end of the regular expression
+}
