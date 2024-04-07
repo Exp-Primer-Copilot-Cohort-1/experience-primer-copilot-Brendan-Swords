c0d19587539763ba7a867b22c8e7d74381f91f97
Brendan Swords <135817043+Brendan-Swords@users.noreply.github.com>
2024-04-07 14:21:06 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,9 @@
+function calculateNumbers(var1, var2)
+{
+    let sum = var1 + var2;
+    let difference = var1 - var2;
+    let product = var1 * var2;
+    let quotient = var1 / var2;
+
+    return {sum, difference, product, quotient};
+}
